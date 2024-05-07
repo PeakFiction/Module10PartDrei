903a4433b49b8beacdd60ce6f2d4bd7341722f4d
“Muhammad <sakhran.t@gmail.com>
2024-05-07 16:44:24 +0700

    Experiment 3.2 Be Creative

--- src/components/chat.rs
+++ src/components/chat.rs
@@ -138,22 +138,23 @@ impl Component for Chat {
     fn view(&self, ctx: &Context<Self>) -> Html {
         let submit = ctx.link().callback(|_| Msg::SubmitMessage);
         html! {
-            <div class="flex w-screen">
-                <div class="flex-none w-56 h-screen bg-gray-100">
-                    <div class="text-xl p-3">{"Users"}</div>
+            <div class="flex w-screen bg-[#0d0d0d] text-[#1bff80] font-mono">
+                // Sidebar with user list
+                <div class="flex-none w-56 h-screen border-r-2 border-[#1bff80]">
+                    <div class="text-xl p-3 uppercase">{"ACTIVE USERS"}</div>
                     {
                         self.users.clone().iter().map(|u| {
                             html!{
-                                <div class="flex m-3 bg-white rounded-lg p-2">
+                                <div class="flex m-3 rounded-lg p-2 border border-[#1bff80]">
                                     <div>
                                         <img class="w-12 h-12 rounded-full" src={u.avatar.clone()} alt="avatar"/>
                                     </div>
                                     <div class="flex-grow p-3">
                                         <div class="flex text-xs justify-between">
-                                            <div>{u.name.clone()}</div>
+                                            <div>{u.name.clone().to_uppercase()}</div>
                                         </div>
-                                        <div class="text-xs text-gray-400">
-                                            {"Hi there!"}
+                                        <div class="text-xs">
+                                            {"VAULT DWELLER"}
                                         </div>
                                     </div>
                                 </div>
@@ -161,20 +162,27 @@ impl Component for Chat {
                         }).collect::<Html>()
                     }
                 </div>
+        
+                // Main chat area
                 <div class="grow h-screen flex flex-col">
-                    <div class="w-full h-14 border-b-2 border-gray-300"><div class="text-xl p-3">{"💬 Chat!"}</div></div>
-                    <div class="w-full grow overflow-auto border-b-2 border-gray-300">
+                    // Chat header
+                    <div class="w-full h-14 border-b-2 border-[#1bff80] flex items-center">
+                        <div class="text-xl p-3">{"DATA RELAY"}</div>
+                    </div>
+        
+                    // Chat messages
+                    <div class="w-full grow overflow-auto border-b-2 border-[#1bff80]">
                         {
                             self.messages.iter().map(|m| {
                                 let user = self.users.iter().find(|u| u.name == m.from).unwrap();
                                 html!{
-                                    <div class="flex items-end w-3/6 bg-gray-100 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg ">
+                                    <div class="flex items-end w-3/6 m-8 rounded-tl-lg rounded-tr-lg rounded-br-lg border border-[#1bff80]">
                                         <img class="w-8 h-8 rounded-full m-3" src={user.avatar.clone()} alt="avatar"/>
                                         <div class="p-3">
-                                            <div class="text-sm">
+                                            <div class="text-sm uppercase">
                                                 {m.from.clone()}
                                             </div>
-                                            <div class="text-xs text-gray-500">
+                                            <div class="text-xs">
                                                 if m.message.ends_with(".gif") {
                                                     <img class="mt-3" src={m.message.clone()}/>
                                                 } else {
@@ -186,12 +194,13 @@ impl Component for Chat {
                                 }
                             }).collect::<Html>()
                         }
-
                     </div>
+        
+                    // Chat input
                     <div class="w-full h-14 flex px-3 items-center">
-                        <input ref={self.chat_input.clone()} type="text" placeholder="Message" class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700" name="message" required=true />
-                        <button onclick={submit} class="p-3 shadow-sm bg-blue-600 w-10 h-10 rounded-full flex justify-center items-center color-white">
-                            <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-white">
+                        <input ref={self.chat_input.clone()} type="text" placeholder="MESSAGE" class="block w-full py-2 pl-4 mx-3 bg-[#0d0d0d] rounded-full outline-none border border-[#1bff80] focus:text-[#1bff80] text-sm" name="message" required=true />
+                        <button onclick={submit} class="p-3 shadow-sm bg-[#1bff80] w-10 h-10 rounded-full flex justify-center items-center">
+                            <svg fill="#000000" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="fill-black">
                                 <path d="M0 0h24v24H0z" fill="none"></path><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"></path>
                             </svg>
                         </button>
--- src/components/login.rs
+++ src/components/login.rs
@@ -28,11 +28,14 @@ pub fn login() -> Html {
     };
 
     html! {
-        <div class="bg-gray-800 flex w-screen">
-            <div class="container mx-auto flex flex-col justify-center items-center	">
-                <form class="m-4 flex">
-                    <input {oninput} class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Username"/>
-                    <Link<Route> to={Route::Chat}> <button {onclick} disabled={username.len()<1} class="px-8 rounded-r-lg bg-violet-600	  text-white font-bold p-4 uppercase border-violet-600 border-t border-b border-r" >{"Go Chatting!"}</button></Link<Route>>
+        <div class="bg-[#0d0d0d] flex w-screen h-screen items-center justify-center">
+            <div class="flex flex-col items-center">
+                <div class="text-3xl text-[#1bff80] mb-8">{"DATA RELAY NETWORK"}</div>
+                <form class="flex flex-col items-center">
+                    <input {oninput} class="rounded-lg p-4 border text-[#1bff80] border-[#1bff80] bg-[#0d0d0d] mb-4 text-center" placeholder="USERNAME" />
+                    <Link<Route> to={Route::Chat}>
+                        <button {onclick} disabled={username.len()<1} class="px-8 rounded-lg bg-[#1bff80] text-black font-bold p-4 uppercase">{"ENTER"}</button>
+                    </Link<Route>>
                 </form>
             </div>
         </div>
